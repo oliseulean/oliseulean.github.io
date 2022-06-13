429e4348c86e2e2ed5458908d0fd33079351c3ba
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2022-06-13 20:20:50 +0300

    Added PageTitle component and global color variables in JS.

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 
+    :style="{ color: props.color }"
+    class="app__title"
+  >
+    <slot />
+  </h1>
+</template>
+
+<script setup>
+/*
+    props
+*/
+
+const props = defineProps({
+  color: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+h1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: transparent;
+  @include font-weight(light);
+  font-size: $font-size-large-l;
+  padding: 2.5rem 0;
+}
+</style>
---
+++ src/helpers/colorsHelper.js
@@ -0,0 +1,17 @@
+export default {
+  colors: [
+    {
+      colorRed: '#ff0000',
+      colorOrangePrimary: '#ffa700',
+      colorGrey: '#64748b',
+      colorDark: '#1e293b',
+      colorWhite: '#ffffff',
+      colorDarkAlt: '#334155',
+      colorLight: '#f1f5f9',
+      colorBluePrimary: '#013299',
+      colorWhiteGray: '#afb0b1',
+      colorShadowGray: '#cfcfcf',
+      colorDarkBlue: '#051622',
+    },
+  ],
+};
