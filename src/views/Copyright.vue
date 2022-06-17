75c455e2e0ca01f2f3f6babd696d3683c5ef518f
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2022-06-17 21:45:43 +0300

    added Copyright component.

---
+++ src/views/Copyright.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="app__footer"
+    v-html="
+      `&copy; Copyright &copy; ${getCurrentYear()} | All rights reserved. <br /> Designed by Olimpiu Șeulean`
+    "
+  />
+</template>
+
+<script setup>
+import getCurrentYear from '../helpers/getCurrentYear';
+</script>
+
+<style scoped lang="scss">
+.app__footer {
+  color: $color-light;
+  background: $color-dark-blue;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  font-size: 0.8rem;
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  padding: 1rem;
+}
+</style>
