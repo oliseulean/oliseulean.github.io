48337a517ab8cae8aeaef3ee86949c84f4d9610d
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2022-06-18 23:49:40 +0300

    added fallback title for routing.

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,30 @@ const router = createRouter({
     {
       path: '/',
       name: 'App',
+      meta: {
+        title: 'Olimpiu Seulean - Frontend Engineer',
+      },
       component: () => import('../layouts/Main.vue'),
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'FourOhFour',
+      meta: {
+        title: 'Error',
+      },
       component: () => import('../components/FourOhFour.vue'),
     },
   ],
+});
+
+router.beforeEach((toRoute, fromRoute, next) => {
+  const title = 'Olimpiu Seulean - Frontend Engineer';
+
+  toRoute?.meta?.title === 'Error'
+    ? (window.document.title = `${toRoute?.meta?.title} | ${title}`)
+    : (window.document.title = title);
+
+  next();
 });
 
 export default router;
