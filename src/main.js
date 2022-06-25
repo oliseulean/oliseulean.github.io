21dd84977759bed6de2bb7481f4801be33c5def6
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2022-06-25 15:15:23 +0300

    Added Vue cookie module.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import VueCookieAcceptDecline from 'vue-cookie-accept-decline';
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component('vue-cookie-accept-decline', VueCookieAcceptDecline);
 
 app.mount('#app');
