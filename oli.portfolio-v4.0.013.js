133e5394bdec81a1455cc263c0bf3871a2770185
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2022-09-25 16:58:36 +0300

    v4.1.0

--- oli.portfolio-v4.0.013.js
+++
@@ -1,3 +0,0 @@
-import{_ as e,b as r,c as t,u as _}from"./oli.portfolio-v4.0.0.js";const a=()=>new Date().getFullYear();const n=["innerHTML"],s={__name:"Copyright",setup(o){return(p,c)=>(r(),t("div",{class:"app-footer",innerHTML:`\xA9 Copyright \xA9 ${_(a)()} | All rights reserved.
-      <br />
-      Designed by Olimpiu \u0218eulean`},null,8,n))}};var l=e(s,[["__scopeId","data-v-16579f78"]]);export{l as default};
