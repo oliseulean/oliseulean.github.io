787fb8740c528da9bec5d340e89e14a611a2cb90
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-05-13 12:41:17 +0300

    typo import.

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -14,7 +14,7 @@ const Work = defineAsyncComponent(() => import('../views/Work.vue'));
 const Technologies = defineAsyncComponent(() => import('../views/Technologies'));
 const Projects = defineAsyncComponent(() => import('../views/Projects.vue'));
 const Copyright = defineAsyncComponent(() => import('../../src/views/Copyright/Copyright.vue'));
-const BackToTop = defineAsyncComponent(() => import('../components/BackToTop/BackToTop.vue'));
+const BackToTop = defineAsyncComponent(() => import('../components/BackToTop.vue'));
 const Cookie = defineAsyncComponent(() => import('../components/Cookie.vue'));
 
 /* Store */
