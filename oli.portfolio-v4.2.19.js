5d1062414bb32abb2c1bb00b1f8a9e6dee5b71f9
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-05-13 12:43:14 +0300

    v4.3.0

--- oli.portfolio-v4.2.19.js
+++
@@ -1,3 +0,0 @@
-import{_ as e,b as t,c as r,u as _}from"./oli.portfolio-v4.2.1.js";const n=()=>new Date().getFullYear();const s=["innerHTML"],o={__name:"Copyright",setup(a){return(c,p)=>(t(),r("div",{class:"app-footer",innerHTML:`© Copyright © ${_(n)()} | All rights reserved.
-      <br />
-      Designed by Olimpiu Șeulean`},null,8,s))}},l=e(o,[["__scopeId","data-v-6d4ed69b"]]);export{l as default};
---
+++ oli.portfolio-v4.3.09.js
@@ -0,0 +1,3 @@
+import{_ as e,b as t,c as r,u as _}from"./oli.portfolio-v4.3.0.js";const n=()=>new Date().getFullYear();const s=["innerHTML"],o={__name:"Copyright",setup(a){return(c,p)=>(t(),r("div",{class:"app-footer",innerHTML:`© Copyright © ${_(n)()} | All rights reserved.
+      <br />
+      Designed by Olimpiu Șeulean`},null,8,s))}},l=e(o,[["__scopeId","data-v-6d4ed69b"]]);export{l as default};
